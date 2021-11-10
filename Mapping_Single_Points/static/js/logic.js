e44c3cb7b928bc1a207d51baad081d98423f229a
LaviJ <lavinajagwani@gmail.com>
2021-11-10 14:12:55 -0800

    Completion of Module 13.4.1

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -3,10 +3,13 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// // Create the map object with a center and zoom level.
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Create the map object with a center and zoom level
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,    
     accessToken: API_KEY
@@ -18,4 +21,18 @@ streets.addTo(map);
 
 //changing the mapbox style
 // mapbox://styles/mapbox/streets-v11
+
+//Module 13.4.1 Map a Single Point
+//Add a Marker to the Map
+//the latitude and longitude for Los Angeles, California, for adding a marker
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//Add a Circle to the Map
+L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: '#ffffal',
+    // fillOpacity: 0.2,
+    radius: 300    
+ }).addTo(map);
 
