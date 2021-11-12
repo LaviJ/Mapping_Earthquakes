c15fa121114339e06412e8fd7ec079721f26d4e9
LaviJ <lavinajagwani@gmail.com>
2021-11-11 16:48:39 -0800

    Completion of Module 13.5.5

---
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -0,0 +1,107 @@
+// Mod 13.5.2 Map GeoJSON Point Type
+
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// 2.	// Create the map object with center and zoom level.
+// let map = L.map('mapid').setView([30, 30], 2);
+
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+// Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//     //adding the oneach function
+//     onEachFeature: function(feature, layer) {
+//       console.log(layer);
+//       layer.bindPopup("<h1>" + "Airport code : " + feature.properties.faa + "----------------------------" + "Airport name: " + feature.properties.name + "</h1>");
+//      }
+// }).addTo(map);
+
+// // We turn each feature into a marker on the map (the below function can be used in place of the above)
+// pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h1>" + feature.properties.name + "</h1>" + "<h2>" + "--------------------------------------------------" + feature.properties.city +", " + feature.properties.country + "</h2>");
+// }
+// }).addTo(map);
+
+let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,    
+    accessToken: API_KEY
+    //mapbox://styles/mapbox/navigation-night-v1
+});
+
+// We create the day view tile layer that will be an option for our map.
+let day = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Night: night,
+    Day: day
+  };
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [44.0, -80.0],
+    zoom: 2,
+    layers: [night]
+})
+
+
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let torontoData = "https://raw.githubusercontent.com/LaviJ/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+    style: myStyle,  
+  //adding the oneach function
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup("<h3> Airport code : " + feature.properties.faa + "</h3> <hr><h3> Airport name: " + feature.properties.name + "</h3>");
+     }
+  }).addTo(map);
+});
+
+
+
+
+  
